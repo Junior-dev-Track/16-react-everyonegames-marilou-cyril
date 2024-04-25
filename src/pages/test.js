d9be895e91661746fc8dece94e96e80edb893c5b
cyril-fl <cyril-f@hotmail.com>
2024-04-25 11:20:07 +0200

    * get init

---
+++ src/pages/test.js
@@ -0,0 +1,11 @@
+import ApiClient from '../script/axios.js';
+import dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+
+// Instanciation de l'ApiClient
+const request = new ApiClient();
+const search = 'platforms';
+
+// Requete
+request.get(search)
