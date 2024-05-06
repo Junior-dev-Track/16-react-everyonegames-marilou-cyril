4022b54b0331da141e879ba7d8fadeda5e776183
cyril-fl <cyril-f@hotmail.com>
2024-05-06 09:26:39 +0200

    -

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,41 +8,42 @@ const Home = ({ list }) => {
 
 
     useEffect(() => {
-
         if (list && list.length > 0) {
             setGameList(list);
             console.log(list)
             const randomIndex = Math.floor(Math.random() * list.length);
             setRandomResult(list[randomIndex]);
         }
-
     }, [list]);
 
 
     return (
         <>
-            <main className="page home">
-                <div className="homeBackground" style={{backgroundImage: `url(${randomResult.background_image})`}}>
-                    <div className="backgroundFilter" />
-                </div>
-                <div className="homeContent">
-                    <Header />
-                    <div className="homeDetails">
-                        {/* Ajout d'une vérification pour s'assurer que randomResult est défini */}
-                        {randomResult && (
-                            <>
-                                <h2 className="homeTitle">{randomResult.name}</h2>
-                                <ul className='homeList'>
-                                    {/* Utilisation de map() seulement si randomResult.genres est un tableau */}
-                                    {randomResult.genres && randomResult.genres.map(genre => (
-                                        <li key={crypto.randomUUID()}>{genre.name}</li>
-                                    ))}
-                                </ul>
-                            </>
-                        )}
+            <main className="home">
+                <section className="page">
+                    <div className="homeBackground" style={{backgroundImage: `url(${randomResult.background_image})`}}>
+                        <div className="backgroundFilter" />
                     </div>
-                    <Dock />
-                </div>
+                    <div className="homeContent">
+                        <Header />
+                        <div className="homeDetails">
+                            {randomResult && (
+                                <>
+                                    <h2 className="homeTitle">{randomResult.name}</h2>
+                                    <ul className='homeList'>
+                                        {/* Utilisation de map() seulement si randomResult.genres est un tableau */}
+                                        {randomResult.genres && randomResult.genres.map(genre => (
+                                            <li key={crypto.randomUUID()}>{genre.name}</li>
+                                        ))}
+                                    </ul>
+                                </>
+                            )}
+                        </div>
+                        <Dock />
+                    </div>
+                </section>
+                {/*<section className="page">*/}
+                {/*</section>*/}
             </main>
         </>
     );
